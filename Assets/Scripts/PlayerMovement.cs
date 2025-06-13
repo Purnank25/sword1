437bca307b812ebd20f80d23973ff79e2fab3eea
Purnank25 <purnankexcel1@gmail.com>
2025-06-13 15:01:16 +0530

    initial commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,21 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float movementInput;
+    private Rigidbody2D playerRB;
+    [Serialize]private float xVelocity = 5.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        movementInput = Input.GetAxis("Horizontal");
+        playerRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerRB.linearVelocityX = movementInput * xVelocity;
     }
 }
